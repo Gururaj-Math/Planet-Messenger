4011bfa3be580ae3298d45dae0e5333364e980bc
samarth2727 <102364425+Gururaj-Math@users.noreply.github.com>
2023-06-29 23:19:58 +0530

    Socket io installed and setup

--- server.js
+++ server.js
@@ -14,3 +14,11 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
+
+//Socket
+
+const io = require('socket.io')(http)
+
+io.on('connection', (socket) => {
+    console.log('Connected Succesfully :)')
+})
